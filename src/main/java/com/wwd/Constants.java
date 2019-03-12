d522d51a7812cdeb191cd0a812666de77e6b45aa
revolver <595009116@qq.com>
2019-03-12 13:46:02 +0800

    添加汪汪队活动

--- src/main/java/com/wwd/Constants.java
+++ src/main/java/com/wwd/Constants.java
@@ -10,4 +10,104 @@ public class Constants {
         main,      // 主图
         normal     //非主图
     }
+    // 汪汪队活动参与1/正常，2/替补，3/退出
+    public enum WwdParticipateStatus{
+        NORMAL("1", "正常"),
+        ALTERNATE("2", "替补"),
+        QUIT("3", "退出");
+
+        private String code;
+        private String name;
+
+        WwdParticipateStatus(String code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public static WwdParticipateStatus getEnumBy(String key) {
+            for (WwdParticipateStatus e : WwdParticipateStatus.values()) {
+                if (e.getCode().equals(key) || e.getName().equals(key)) {
+                    return e;
+                }
+            }
+            return null;
+        }
+    }
+
+
+    /**
+     * 活动审核（未审核，1，审核成功，2，审核失败，3）
+     */
+    public enum ActivityReview {
+        NONE("1", "未审核"),
+        SUCCESS("2", "审核成功"),
+        FAIL("3", "审核失败");
+
+        private String code;
+        private String name;
+
+        ActivityReview(String code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public static ActivityReview getEnumBy(String key) {
+            for (ActivityReview e : ActivityReview.values()) {
+                if (e.getCode().equals(key) || e.getName().equals(key)) {
+                    return e;
+                }
+            }
+            return null;
+        }
+    }
+
+    /**
+     * 活动状态（1，报名中，2，名额满，3，已结束）
+     */
+    public enum ActivityStatus {
+        APPLYING("1", "报名中"),
+        QUOTA_FULL("2", "名额满"),
+        END("3", "已结束");
+
+        private String code;
+        private String name;
+
+        ActivityStatus(String code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public static ActivityStatus getEnumBy(String key) {
+            for (ActivityStatus e : ActivityStatus.values()) {
+                if (e.getCode().equals(key) || e.getName().equals(key)) {
+                    return e;
+                }
+            }
+            return null;
+        }
+    }
 }
